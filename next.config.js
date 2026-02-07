a93a14ef3e510d4e9cb2780ac90779dbcf9d9a41
stefan.ignat <stefanignat26@gmail.com>
2026-02-07 15:34:28 +0200

    chore: update configuration and dependencies
    - Refactor `next.config.js` for improved readability and added new image remote patterns.
    - Change project name in `package-lock.json` and `package.json` from "medusa-next" to "v2-solidon-frontend".
    - Clean up unused styles and configurations in `tailwind.config.js`.
    - Update product info component for better structure and readability.

--- next.config.js
+++ next.config.js
@@ -1,58 +1,90 @@
-const checkEnvVariables = require("./check-env-variables")
+const checkEnvVariables = require("./check-env-variables");
 
-checkEnvVariables()
+checkEnvVariables();
 
 /**
  * Medusa Cloud-related environment variables
  */
-const S3_HOSTNAME = process.env.MEDUSA_CLOUD_S3_HOSTNAME
-const S3_PATHNAME = process.env.MEDUSA_CLOUD_S3_PATHNAME
+const S3_HOSTNAME = process.env.MEDUSA_CLOUD_S3_HOSTNAME;
+const S3_PATHNAME = process.env.MEDUSA_CLOUD_S3_PATHNAME;
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  reactStrictMode: true,
-  logging: {
-    fetches: {
-      fullUrl: true,
+    reactStrictMode: true,
+    logging: {
+        fetches: {
+            fullUrl: true,
+        },
     },
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
-      },
-      ...(S3_HOSTNAME && S3_PATHNAME
-        ? [
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        remotePatterns: [
             {
-              protocol: "https",
-              hostname: S3_HOSTNAME,
-              pathname: S3_PATHNAME,
+                protocol: "http",
+                hostname: "localhost",
             },
-          ]
-        : []),
-    ],
-  },
-}
+            {
+                protocol: "https",
+                hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
+            },
+            {
+                protocol: "https",
+                hostname: "medusa-server-testing.s3.amazonaws.com",
+            },
+            {
+                protocol: "https",
+                hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
+            },
+            {
+                protocol: "https",
+                hostname: "marketplace-static.emag.ro",
+            },
+            {
+                protocol: "https",
+                hostname: "bucket-doc-s1.s3.eu-central-1.amazonaws.com",
+            },
+            {
+                protocol: "https",
+                hostname: "*.akamaized.net",
+            },
+            {
+                protocol: "https",
+                hostname: "i.ibb.co",
+            },
+            {
+                protocol: "https",
+                hostname: "static.emag.ro",
+            },
+            {
+                protocol: "https",
+                hostname: "*.emag.ro",
+            },
+            {
+                protocol: "https",
+                hostname: "*.emag.net",
+            },
+            {
+                protocol: "https",
+                hostname: "upload.cdn.baselinker.com",
+            },
+            ...(S3_HOSTNAME && S3_PATHNAME
+                ? [
+                      {
+                          protocol: "https",
+                          hostname: S3_HOSTNAME,
+                          pathname: S3_PATHNAME,
+                      },
+                  ]
+                : []),
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
